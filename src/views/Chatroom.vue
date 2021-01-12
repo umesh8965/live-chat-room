2a36addfbb4c000ed92d9d8b65472c5eb792e35a
umesh8965 <umesh_8965@yahoo.co.in>
2021-01-12 12:18:14 +0530

    firebase auth

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,12 +5,27 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import { useRouter } from 'vue-router'
 
 import Navbar from "../components/Navbar"
+import getUser from "../composables/getUser"
 
 export default {
 
-    components: { Navbar }
+    components: { Navbar },
+    setup(){
+      const { user } = getUser()
+      const router = useRouter()
+
+      watch(user, () => {
+        if(!user.value){
+          router.push({ name: "Welcome" })
+        }
+      })
+
+      return { user }
+    }
 
 }
 </script>
